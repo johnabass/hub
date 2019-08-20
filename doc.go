2cafeb0894b8b74967b5d5f90c9c813b450be385
johnabass <johnbass1017@yahoo.com>
2019-08-19 20:41:50 -0700

    Fixed a typo

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 // Package hub implements a simple publish/subscribe API for application events.  Different subsystems
 // within an application can send notifications without having hard dependencies on each other.
 //
-// Dependency injection approaches often benefit from this approach.  Examples include sending server
+// Dependency injection often benefits from this approach.  Examples include sending server
 // start and stop events to other components and service discovery events from libraries like consul.
 //
 // This package is an example of the Mediator design pattern (see https://en.wikipedia.org/wiki/Mediator_pattern).
